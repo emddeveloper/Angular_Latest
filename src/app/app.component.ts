755921dd9d30122b484f38a77be35f725c6671ab
emddeveloper <emd.developer@gmail.com>
2021-03-16 16:03:23 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+
+  passwordLength(value: string) {
+    this.length = parseInt(value);
+  }
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  onButtonClick() {
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const numbers = '1234567890';
+    const symbols = '!@#$%^&*()';
+
+    let validChar = '';
+    if (this.includeLetters) {
+      validChar += letters;
+    }
+    if (this.includeNumbers) {
+      validChar += numbers;
+    }
+    if (this.includeSymbols) {
+      validChar += symbols;
+    }
+    let generatePassword = '';
+    for (let i = 0; i < this.length; i++) {
+      let index = Math.floor(Math.random() * validChar.length);
+      generatePassword += validChar[index];
+    }
+    this.password = generatePassword;
+  }
+  getPassword() {
+    return this.password;
+  }
 }
